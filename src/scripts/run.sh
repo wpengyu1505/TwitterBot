c9db53ba512401ba309c245534b28c18253c411c
wpy <wpy@10.0.0.240>
2016-07-23 17:07:07 -0400

    adding script

---
+++ src/scripts/run.sh
@@ -0,0 +1,11 @@
+QUERY=$1
+DATE=$2
+LIMIT=$3
+OUTPUT=$4
+
+java -cp twitterbot-0.0.1-SNAPSHOT.jar:twitter4j-core-4.0.4.jar wpy.twitterbot.sentiment.Runner \
+    $QUERY \
+    $DATE \
+    $LIMIT \
+    $OUTPUT
+
